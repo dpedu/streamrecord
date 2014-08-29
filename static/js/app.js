dfd5b1dbe7475f0e5ff72437a75b5bd761df1c22
dave <dave@davepedu.com>
2014-08-29 08:42:08 -0700

    Divide minutes correctly

--- static/js/app.js
+++ static/js/app.js
@@ -168,7 +168,7 @@ var behaviors = {
 					}).on("slide", function(){
 						newTime = $(".time-holder .player-slider").slider('getValue').val();
 						hours =  Math.floor(newTime/60/60)
-						minutes = Math.floor(newTime/60)
+						minutes = Math.floor(newTime/60)%60
 						seconds = Math.floor(newTime-(minutes*60))
 						$(".timeshow").html((hours>0?hours+":":"")+minutes+":"+(seconds<10?"0":"")+seconds)
 					});
@@ -186,7 +186,7 @@ var behaviors = {
 					// And update the time
 					newTime = event.jPlayer.status.currentTime;
 					hours =  Math.floor(newTime/60/60)
-					minutes = Math.floor(newTime/60)
+					minutes = Math.floor(newTime/60)%60
 					seconds = Math.floor(newTime-(minutes*60))
 					$(".timeshow").html((hours>0?hours+":":"")+minutes+":"+(seconds<10?"0":"")+seconds)
 				}
